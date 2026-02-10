e53443d79a6a0231d5115cd986a59ea997d2ba1f
Celes Renata <celes@frameshift.net>
2026-02-09 21:48:30 -0800

    Fix tinygrad model loading: assign to model/tokenizer variables
    The tinygrad model loading was assigning to tinygrad_model/tinygrad_tokenizer
    but the code later expects model/tokenizer variables. This caused an
    AssertionError when StartWarmup task tried to use the model.

--- src/exo/worker/runner/runner.py
+++ src/exo/worker/runner/runner.py
@@ -301,7 +301,7 @@ def main(
                                     )
 
                                     # Load model and tokenizer
-                                    tinygrad_model, tinygrad_tokenizer = (
+                                    model, tokenizer = (
                                         loop.run_until_complete(
                                             load_tinygrad_model(
                                                 shard_metadata,
